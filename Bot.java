8caba3c403c98b73666248457bd07b1a8c857e9f
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-05-02 11:35:02 +0200

    add: finished exercise

--- Bot.java
+++ Bot.java
@@ -1,33 +1,41 @@
+import java.util.Scanner;
+
 public class Bot {
     public static void main(String[] args) {
         //Crea el scanner
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hola, ¿Cómo te llamas?");
 
         //Recoge el dato
-        //Añade una nueva línea antes de mostrar la siguiente pregunta.
+        String name = scanner.nextLine();
 
-        System.out.println("Hola <name>! Soy FemBot. ¿De qué ciudad eres?");
+        //Añade una nueva línea antes de mostrar la siguiente pregunta.
+        System.out.print("¡Espero que estés bien! ");
+        System.out.println("Hola " +name+"! Soy FemBot. ¿De qué ciudad eres?");
 
         //Recoge el dato
+        String location = scanner.nextLine();
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
-
-        System.out.println("He escuchado que <location> es un encanto. Yo nací en Oracle city.");
-        System.out.println("\n¿Cuántos años tienes?");
+        System.out.print("¡Qué bien! ");
+        System.out.println("He escuchado que "+location+" es un encanto. Yo nací en Oracle city. ");
+        System.out.println("¿Cuántos años tienes?");
 
         //Recoge el dato
+        double age = Double.parseDouble (scanner.nextLine());
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
-
-        System.out.println("Entonces me dices que tienes <age> años. Yo tengo 400 años.");
-        System.out.println("Eso quiere decir que soy <divide 400 por tu edad> veces más vieja que tu.");
+        System.out.print("¡Wow!");
+        System.out.println("Entonces me dices que tienes "+age+" años. Yo tengo 400 años.");
+        System.out.println("Eso quiere decir que soy "+(400.0/age)+" veces más vieja que tu.");
         System.out.println("Basta de hablar de mi misma. ¿Cuál es tu lenguaje de programación favorito? (Por favor no digas Python)");
 
         //Recoge el dato
+        String language = scanner.nextLine();
         //Añade una nueva línea.
-        System.out.println("<language>, ¡Eso es excelente! Mucho gusto haber hablado contigo <name>, ¡Hablamos luego!");
-
+        System.out.println(""+language+", ¡Eso es excelente! Mucho gusto haber hablado contigo "+name+", ¡Hablamos luego!");
+        System.out.println("O nunca ;) ");
         //¿Te acordaste del nextLine trap?
         //Recuerda cerrar el scanner
+        scanner.close();
 
     }
 }
--- SignUp.java
+++ SignUp.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
+
 public class SignUp {
     public static void main(String[] args) {
         //Vamos a crear un formulario de registro
-
+        Scanner scanner = new Scanner(System.in);
         //Los datos que debes pedir al usuario son:
         //1. Nombre
         //2. Apellido
@@ -9,9 +11,18 @@ public class SignUp {
         //4. Contraseña
 
         //Añade una nueva línea antes de mostrar la siguiente pregunta.
-
+        System.out.println("Hola, ¿Cuál es tu nombre?");
+        String name = scanner.nextLine();
+        System.out.println("¿Cuál es tu apellido?");
+        String surname = scanner.nextLine();
+        System.out.println("¿Qué nombre de usuario deseas poner?");
+        String user = scanner.nextLine();
+        System.out.println("Introduce la contraseña deseada");
+        String password = scanner.nextLine();
         //Imprimir el siguiente resultado:
-        //Hola <nombre> <apellido>, tu nombre de usuario es <usuario> y tu contraseña es <contraseña>, gracias por registrarte.
+        System.out.println("A continuación, se muestra el registro realizado: ");
+        System.out.println("Hola " +name+" "+ surname+", tu nombre de usuario es " +user+" y tu contraseña es "+password+", gracias por registrarte.");
         //Añade una nueva línea antes de mostrar la respuesta
+        scanner.close();
     }
 }
